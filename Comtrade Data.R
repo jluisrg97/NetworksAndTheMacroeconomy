0594ab7115e55e07b0aac633c2982ca8fe17efcb
José Luis Rodríguez-Guzmán <jlrodzg@gmail.com>
2020-07-09 14:47:58 -0500

    Extraer datos de Comtrade

---
+++ Comtrade Data.R
@@ -0,0 +1,16 @@
+#####################################################################################################
+##
+## Networks and the Macroeconomy: An Empirical Approach - UN Comtrade Data
+## 
+## Data: UN Comtrade
+## Author: José Luis Rodríguez-Guzmán & Nathaly Parra-Nava
+## Date: June 25th, 2020
+##
+#####################################################################################################
+
+## Settings
+
+# Libs----
+
+library(comtradr) # An API for UN Comtrade Data
+
